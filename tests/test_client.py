b8e005a7164acbd33559c0c931a77e4c3a6f625e
Eugene <eighthcolor22@gmail.com>
2024-03-19 16:58:03 +0200

    Fix tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -782,7 +782,6 @@ async def test_subscribe_to_topic_with_incorrect(fake_async_fcm_client_w_creds, 
 @pytest.mark.parametrize("fake_multi_device_tokens", (3,), indirect=True)
 async def test_unsubscribe_to_topic(fake_async_fcm_client_w_creds, fake_multi_device_tokens, httpx_mock: HTTPXMock):
     fake_async_fcm_client_w_creds._get_access_token = fake__get_access_token
-    creds = fake_async_fcm_client_w_creds._credentials
     httpx_mock.add_response(
         status_code=200,
         json={"results": [{}, {}, {}]},
@@ -797,7 +796,7 @@ async def test_unsubscribe_to_topic(fake_async_fcm_client_w_creds, fake_multi_de
 
 
 @pytest.mark.parametrize("fake_multi_device_tokens", (3,), indirect=True)
-async def test_subscribe_to_topic_with_incorrect(fake_async_fcm_client_w_creds, fake_multi_device_tokens, httpx_mock: HTTPXMock):
+async def test_unsubscribe_to_topic_with_incorrect(fake_async_fcm_client_w_creds, fake_multi_device_tokens, httpx_mock: HTTPXMock):
     fake_async_fcm_client_w_creds._get_access_token = fake__get_access_token
     creds = fake_async_fcm_client_w_creds._credentials
 
